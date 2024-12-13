a2d944900b43460ecb00243f90af186733b5842e
hannakortetjarvi <kortetjarvihanna@gmail.com>
2024-12-13 19:23:40 +0200

    Advent of Code 2024 Day 6 Complete

--- 2024/day6.pl
+++ 2024/day6.pl
@@ -7,19 +7,19 @@ open($INFILE, "inputs/day6.txt") or die "day6.txt doesn't open: $!";
 
 my @obstacles;
 my $length = 0;
-my $x;
-my $y;
+my $start_x;
+my $start_y;
 while (my $line = <$INFILE>) {
     chomp $line;
     for my $i (0 .. length($line) - 1) {
         if (substr($line, $i, 1) eq '#') {
-            my $pair = $i . '|' . $length;
+            my $pair = "($i|$length)";
             push @obstacles, $pair;
         }
 
         if (substr($line, $i, 1) eq '^') {
-            $x = $i;
-            $y = $length;
+            $start_x = $i;
+            $start_y = $length;
         }
     }
     $length++;
@@ -27,46 +27,87 @@ while (my $line = <$INFILE>) {
 
 # ------ PART 1 ------
 
-my @directions = (0, 1, 2, 3); # up, right, down, left
-my $current_dir = 0;
-my @visited_locations;
+my %obstacle_hash = map { $_ => 1 } @obstacles;
+my %visited;
 
-my $result = 0;
-while ($x >= 0 && $x <= $length && $y >= 0 && $y <= $length) {
-    my $pair = $x . '|' . $y;
-    push @visited_locations, $pair;
+$current_dir = 0;
+$x = $start_x;
+$y = $start_y;
 
-    my $next_x = $x;
-    my $next_y = $y;
+$result = 0;
+while ($x >= 0 && $x < $length && $y >= 0 && $y < $length) {
+    my $pair = "($x|$y)";
+
+    my ($next_x, $next_y) = ($x, $y);
     if ($current_dir == 0) {
         $next_y--;
-    }
-    elsif ($current_dir == 1) {
+    } elsif ($current_dir == 1) {
         $next_x++;
-    }
-    elsif ($current_dir == 2) {
+    } elsif ($current_dir == 2) {
         $next_y++;
-    }
-    else {
+    } else {
         $next_x--;
     }
 
-    my $pattern = "$next_x|$next_y";
-    if ( grep {$_ eq $pattern} @obstacles ) {
+    my $next_pair = "($next_x|$next_y)";
+    if (exists $obstacle_hash{$next_pair}) {
         $current_dir = ($current_dir + 1) % 4;
         next;
     }
 
-    if ( not(grep {$_ eq $pattern} @visited_locations) ) {
-        push @visited_locations, $pair;
+    if (!exists $visited{$pair}) {
+        $visited{$pair} = 1; # Mark as visited
         $result++;
     }
 
-    $x = $next_x;
-    $y = $next_y;
+    ($x, $y) = ($next_x, $next_y);
 }
 
 print "Answer to part 1: $result";
-#4662 TOO LOW
 
 # ------ PART 2 ------
+
+%obstacle_hash = map { $_ => 1 } @obstacles;
+
+$result = 0;
+foreach my $pair (%visited) {
+    my %visited;
+    my $current_dir = 0;
+    $x = $start_x;
+    $y = $start_y;
+
+    $obstacle_hash{$pair} = 1;
+    while ($x >= 0 && $x < $length && $y >= 0 && $y < $length) {
+        my $loc = "($x|$y)|$current_dir";
+
+        if ($visited{$loc}) {
+            $result++;
+            last;
+        }
+
+        my ($next_x, $next_y) = ($x, $y);
+        if ($current_dir == 0) {
+            $next_y--;
+        } elsif ($current_dir == 1) {
+            $next_x++;
+        } elsif ($current_dir == 2) {
+            $next_y++;
+        } else {
+            $next_x--;
+        }
+
+        my $next_pos = "($next_x|$next_y)";
+        if (exists $obstacle_hash{$next_pos}) {
+            $current_dir = ($current_dir + 1) % 4;
+            next;
+        }
+
+        $visited{$loc} = 1; # Mark as visited
+
+        ($x, $y) = ($next_x, $next_y);
+    }
+
+    delete $obstacle_hash{$pair};
+}
+
+print "Answer to part 2: $result";
