2551d4a654c25a9b1fbad575b9c52c6c5fc94529
hannakortetjarvi <kortetjarvihanna@gmail.com>
2024-12-13 20:29:49 +0200

    Advent of Code 2024 Day 7 Complete

---
+++ 2024/day7.pl
@@ -0,0 +1,81 @@
+#!/usr/bin/perl -l
+use warnings;
+
+# Read lines into matrix
+my $INFILE;
+open($INFILE, "inputs/day7.txt") or die "day7.txt doesn't open: $!";
+
+my $calibration = 0;
+while (my $line = <$INFILE>) {
+    chomp $line;
+
+    my ($total, $calc) = split ':', $line;
+    $calc =~ s/^\s+//;
+    my @nums = split / +/, $calc;
+
+    my @results;
+    foreach my $num (@nums) {
+        if (scalar(@results) == 0) {
+            push @results, $num;
+            next;
+        }
+
+        my @new_results;
+        for my $i (0 .. $#results) {
+            my $sum = $results[$i] + $num;
+            my $mult = $results[$i] * $num;
+            push @new_results, $sum;
+            push @new_results, $mult;
+        }
+        @results = @new_results;
+    }
+
+    foreach my $result (@results) {
+        if ($result == $total) {
+            $calibration += $result;
+            last;
+        }
+    }
+}
+
+print "Answer to part 1: $calibration\n";
+
+# ----------------------------
+
+open($INFILE, "inputs/day7.txt") or die "day7.txt doesn't open: $!";
+$calibration = 0;
+while (my $line = <$INFILE>) {
+    chomp $line;
+
+    my ($total, $calc) = split ':', $line;
+    $calc =~ s/^\s+//;
+    my @nums = split / +/, $calc;
+
+    my @results;
+    foreach my $num (@nums) {
+        if (scalar(@results) == 0) {
+            push @results, $num;
+            next;
+        }
+
+        my @new_results;
+        for my $i (0 .. $#results) {
+            my $sum = $results[$i] + $num;
+            my $mult = $results[$i] * $num;
+            my $conc = $results[$i] . $num;
+            push @new_results, $sum;
+            push @new_results, $mult;
+            push @new_results, $conc;
+        }
+        @results = @new_results;
+    }
+
+    foreach my $result (@results) {
+        if ($result == $total) {
+            $calibration += $result;
+            last;
+        }
+    }
+}
+
+print "Answer to part 2: $calibration";
